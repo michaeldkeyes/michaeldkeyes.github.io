1ad43431965e5ced173fb8065ddf89384ab85dc6
Michael Keyes <michael.d.keyes87@gmail.com>
2020-08-10 20:27:37 -0400

    Changing theme

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let theme = localStorage.getItem('theme');
+
+if (theme === null) {
+  setTheme('light');
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.querySelectorAll('.theme-dot');
+
+for (let i = 0; i < themeDots.length; i++) {
+  themeDots[i].addEventListener('click', function () {
+    let mode = this.dataset.mode;
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode === 'light') {
+    document.querySelector('#theme-style').href = 'css/default.css';
+  }
+  if (mode === 'blue') {
+    document.querySelector('#theme-style').href = 'css/blue.css';
+  }
+  if (mode === 'green') {
+    document.querySelector('#theme-style').href = 'css/green.css';
+  }
+  if (mode === 'purple') {
+    document.querySelector('#theme-style').href = 'css/purple.css';
+  }
+
+  localStorage.setItem('theme', mode);
+}
